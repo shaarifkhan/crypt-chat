654ced60badd6b123c08539eec2067ffdf7ba296
Shaarif <shaarifkhan999@gmail.com>
2020-04-20 00:44:28 +0500

    add contact component

--- frontend/screens/Addcontact.js
+++ frontend/screens/Addcontact.js
@@ -1,26 +1,33 @@
-import React from 'react';
-import { StyleSheet, TextInput, View, Text ,TouchableWithoutFeedback,Keyboard} from 'react-native';
-import { globalStyles } from '../styles/global.js';
-import { Formik } from 'formik';
-import * as yup from 'yup';
+import React from "react";
+import {
+  StyleSheet,
+  TextInput,
+  View,
+  Text,
+  TouchableWithoutFeedback,
+  Keyboard,
+} from "react-native";
+import { globalStyles } from "../styles/global.js";
+import { Formik } from "formik";
+import * as yup from "yup";
 import axios from "axios";
-import { ActivityIndicator } from 'react-native';
-import Icon from 'react-native-vector-icons/FontAwesome';
-import { Input, Button} from 'react-native-elements';
-import Header from '../shared/header';
+import { ActivityIndicator } from "react-native";
+import Icon from "react-native-vector-icons/FontAwesome";
+import { Input, Button } from "react-native-elements";
+import Header from "../shared/header";
 
 const ContactSchema = yup.object({
-    name: yup.string()
-        .required(),
-    email: yup.string(),
-    contact: yup.string()
+  name: yup.string().required(),
+  email: yup.string(),
+  contact: yup.string(),
 });
 
-export default function Contact({ navigation ,addcontact}) {
-    const submitToserver = (values) => {
+export default function AddContact({ navigation, addcontact }) {
+  const submitToserver = (values) => {
     console.log("hello world");
     const { name, email, contact } = values;
-    axios.post("http://192.168.1.105:3000/register", {
+    axios
+      .post("http://192.168.1.105:3000/register", {
         name: name,
         email: email,
         contact: contact,
@@ -31,91 +38,85 @@ export default function Contact({ navigation ,addcontact}) {
       .catch((err) => {
         console.log(err);
       });
-    };
-    return(
-        <View>
-        <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+  };
+  return (
+    <View>
+      <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
         <View style={styles.container}>
-            <Formik
+          <Formik
             initialValues={{
-                name: "",
-                email: "",
-                contact: "",
+              name: "",
+              email: "",
+              contact: "",
             }}
             validationSchema={ContactSchema}
             onSubmit={(values, actions) => {
-                console.log(values);
-                addcontact(values)
-                //submitToserver(values);
-                navigation.navigate("Home");
+              console.log(values);
+              addcontact(values);
+              //submitToserver(values);
+              navigation.navigate("Home");
             }}
-            >
+          >
             {(props) => (
-                <View>
-                    <Input
-                        leftIcon={
-                        <Icon
-                            name='user'
-                            size={24}
-                            color='black'
-                        />}
-              style={globalStyles.input} 
-              placeholder=' Full name'
-              onChangeText={props.handleChange('name')}
-              onBlur={props.handleBlur('name')}
-              value={props.values.name}
+              <View>
+                <Input
+                  leftIcon={<Icon name="user" size={24} color="black" />}
+                  style={globalStyles.input}
+                  placeholder=" Full name"
+                  onChangeText={props.handleChange("name")}
+                  onBlur={props.handleBlur("name")}
+                  value={props.values.name}
                 />
-                <Text style={globalStyles.errorText}>{props.touched.name && props.errors.name}</Text> 
+                <Text style={globalStyles.errorText}>
+                  {props.touched.name && props.errors.name}
+                </Text>
 
-                    <Input 
-                        leftIcon={
-                        <Icon
-                            name='envelope'
-                            size={24}
-                            color='black'
-                        />}
-                    style={globalStyles.input}
-                    placeholder=' Email'
-                    onChangeText={props.handleChange('email')}
-                    onBlur={props.handleBlur('email')} 
-                    value={props.values.rating}
-                    />
-                <Text style={globalStyles.errorText}>{props.touched.email && props.errors.email}</Text>
-                    <Input
-                        leftIcon={
-                        <Icon
-                            name='book'
-                            size={24}
-                            color='black'
-                        />}
-                    style={globalStyles.input}
-                    placeholder=' Contact Number'
-                    onChangeText={props.handleChange('contact')}
-                    onBlur={props.handleBlur('contact')}
-                    value={props.values.password}
-                    />
-                <Text style={globalStyles.errorText}>{props.touched.contact && props.errors.contact}</Text>
-            
-                <Button color='maroon' title="Add" onPress={props.handleSubmit} /> 
-            </View>
+                <Input
+                  leftIcon={<Icon name="envelope" size={24} color="black" />}
+                  style={globalStyles.input}
+                  placeholder=" Email"
+                  onChangeText={props.handleChange("email")}
+                  onBlur={props.handleBlur("email")}
+                  value={props.values.rating}
+                />
+                <Text style={globalStyles.errorText}>
+                  {props.touched.email && props.errors.email}
+                </Text>
+                <Input
+                  leftIcon={<Icon name="book" size={24} color="black" />}
+                  style={globalStyles.input}
+                  placeholder=" Contact Number"
+                  onChangeText={props.handleChange("contact")}
+                  onBlur={props.handleBlur("contact")}
+                  value={props.values.password}
+                />
+                <Text style={globalStyles.errorText}>
+                  {props.touched.contact && props.errors.contact}
+                </Text>
+
+                <Button
+                  color="maroon"
+                  title="Add"
+                  onPress={props.handleSubmit}
+                />
+              </View>
             )}
-            </Formik>
+          </Formik>
         </View>
-        </TouchableWithoutFeedback>
-        </View>
-        );
+      </TouchableWithoutFeedback>
+    </View>
+  );
 }
 const styles = StyleSheet.create({
-    container: {
-      marginTop: 20,   
-      padding: 10,
-      justifyContent: 'center' 
-    },
-    titleText:{
-        fontSize: 55,
-        marginTop:50,
-        marginLeft:45,
-        justifyContent: 'center'
-    }
-
-})
+  container: {
+    marginTop: 20,
+    padding: 10,
+    justifyContent: "center",
+  },
+  titleText: {
+    fontSize: 55,
+    marginTop: 50,
+    marginLeft: 45,
+    justifyContent: "center",
+  },
+});
--- frontend/screens/Home.js
+++ frontend/screens/Home.js
@@ -1,4 +1,4 @@
-import React, {useState } from 'react';
+import React, { useState } from "react";
 import {
   StyleSheet,
   Text,
@@ -8,92 +8,156 @@ import {
   Alert,
   ScrollView,
   FlatList,
-  Modal
-} from 'react-native';
-import Header from '../shared/header';
-import { MaterialIcons } from '@expo/vector-icons';
-import Contactform from './Addcontact'
-export default function Home ({navigation}) {
+  Modal,
+} from "react-native";
+import Header from "../shared/header";
+import { MaterialIcons } from "@expo/vector-icons";
+import AddContact from "./Addcontact";
+export default function Home({ navigation }) {
+  const [contacts, setContacts] = useState([
+    {
+      id: 1,
+      name: "Fawaz Ansari",
+      status: "active",
+      image:
+        "https://scontent.fkhi6-1.fna.fbcdn.net/v/t1.0-9/s960x960/86490745_10216871382282325_2101349845200535552_o.jpg?_nc_cat=103&_nc_sid=85a577&_nc_ohc=9PfyP7Qn_yEAX98rA3s&_nc_ht=scontent.fkhi6-1.fna&_nc_tp=7&oh=90e4beb69a20a3054d0c0f632753bc22&oe=5EC2F9B7",
+    },
+    {
+      id: 2,
+      name: "Yahya Zuberi",
+      status: "active",
+      image:
+        "https://scontent.fkhi6-1.fna.fbcdn.net/v/t1.0-9/71756573_10216348027757629_612090860393201664_n.jpg?_nc_cat=108&_nc_sid=85a577&_nc_ohc=4v2bQle5oAkAX8xGmx2&_nc_ht=scontent.fkhi6-1.fna&oh=3f01386abf6150ac32a062732ffb6a25&oe=5EC14F0F",
+    },
+    {
+      id: 3,
+      name: "Shaarif Khan",
+      status: "active",
+      image:
+        "https://www.pngfind.com/pngs/m/110-1102775_download-empty-profile-hd-png-download.png",
+    },
+    {
+      id: 4,
+      name: "Usman Hussain",
+      status: "active",
+      image:
+        "https://www.pngfind.com/pngs/m/110-1102775_download-empty-profile-hd-png-download.png",
+    },
+    {
+      id: 5,
+      name: "Azhan Ali",
+      status: "active",
+      image:
+        "https://www.pngfind.com/pngs/m/110-1102775_download-empty-profile-hd-png-download.png",
+    },
+    {
+      id: 6,
+      name: "Osama Rajput",
+      status: "active",
+      image:
+        "https://www.pngfind.com/pngs/m/110-1102775_download-empty-profile-hd-png-download.png",
+    },
+    {
+      id: 8,
+      name: "Arbaz Khan",
+      status: "active",
+      image:
+        "https://www.pngfind.com/pngs/m/110-1102775_download-empty-profile-hd-png-download.png",
+    },
+    {
+      id: 9,
+      name: "Fahad Lodi",
+      status: "active",
+      image:
+        "https://www.pngfind.com/pngs/m/110-1102775_download-empty-profile-hd-png-download.png",
+    },
+    {
+      id: 10,
+      name: "Taha Farooqui",
+      status: "active",
+      image:
+        "https://www.pngfind.com/pngs/m/110-1102775_download-empty-profile-hd-png-download.png",
+    },
+    {
+      id: 11,
+      name: "Shaheryar Khalid",
+      status: "active",
+      image:
+        "https://www.pngfind.com/pngs/m/110-1102775_download-empty-profile-hd-png-download.png",
+    },
+  ]);
 
-    const [contacts, setContacts]= useState([
-        {id:1,  name: "Fawaz Ansari",    status:"active", image:'https://scontent.fkhi6-1.fna.fbcdn.net/v/t1.0-9/s960x960/86490745_10216871382282325_2101349845200535552_o.jpg?_nc_cat=103&_nc_sid=85a577&_nc_ohc=9PfyP7Qn_yEAX98rA3s&_nc_ht=scontent.fkhi6-1.fna&_nc_tp=7&oh=90e4beb69a20a3054d0c0f632753bc22&oe=5EC2F9B7'},
-        {id:2,  name: "Yahya Zuberi",   status:"active", image:"https://scontent.fkhi6-1.fna.fbcdn.net/v/t1.0-9/71756573_10216348027757629_612090860393201664_n.jpg?_nc_cat=108&_nc_sid=85a577&_nc_ohc=4v2bQle5oAkAX8xGmx2&_nc_ht=scontent.fkhi6-1.fna&oh=3f01386abf6150ac32a062732ffb6a25&oe=5EC14F0F"} ,
-        {id:3,  name: "Shaarif Khan",  status:"active", image:"https://www.pngfind.com/pngs/m/110-1102775_download-empty-profile-hd-png-download.png"} ,
-        {id:4,  name: "Usman Hussain",  status:"active", image:"https://www.pngfind.com/pngs/m/110-1102775_download-empty-profile-hd-png-download.png"} ,
-        {id:5,  name: "Azhan Ali",   status:"active", image:"https://www.pngfind.com/pngs/m/110-1102775_download-empty-profile-hd-png-download.png"} ,
-        {id:6,  name: "Osama Rajput", status:"active", image:"https://www.pngfind.com/pngs/m/110-1102775_download-empty-profile-hd-png-download.png"} ,
-        {id:8,  name: "Arbaz Khan", status:"active", image:"https://www.pngfind.com/pngs/m/110-1102775_download-empty-profile-hd-png-download.png"} ,
-        {id:9,  name: "Fahad Lodi",    status:"active", image:"https://www.pngfind.com/pngs/m/110-1102775_download-empty-profile-hd-png-download.png"} ,
-        {id:10, name: "Taha Farooqui",  status:"active", image:"https://www.pngfind.com/pngs/m/110-1102775_download-empty-profile-hd-png-download.png"} ,
-        {id:11, name: "Shaheryar Khalid",   status:"active", image:"https://www.pngfind.com/pngs/m/110-1102775_download-empty-profile-hd-png-download.png"},
-    ])
+  const addcontact = (values) => {
+    console.log({ values });
+    values.key = Math.random().toString();
+    values.status = "active";
+    values.image =
+      "https://www.pngfind.com/pngs/m/110-1102775_download-empty-profile-hd-png-download.png";
+    setContacts((currentcontact) => {
+      return [values, ...currentcontact];
+    });
+    setModal(false);
+  };
 
-    const addcontact=(values)=>{
-      console.log({values})
-      values.key= Math.random().toString()
-      values.status= 'active'
-      values.image = "https://www.pngfind.com/pngs/m/110-1102775_download-empty-profile-hd-png-download.png"
-      setContacts((currentcontact)=>{
-        return[values,...currentcontact]
-      })
-      setModal(false)
-    }
-
-    const[modal , setModal]= useState(false)
-    return(
-        <View style={{ flex: 1 }}>
-          <Modal visible={modal} animationType='slide'>
-            <View style={styles.modalContent}>
-              <MaterialIcons 
-              name='close'
-              size={24} 
-              style={{...styles.modalToggle, ...styles.modalClose}} 
-              onPress={() => setModal(false)} 
-              />
-              <Contactform addcontact={addcontact}/>
-            </View>
-          </Modal>
-
-        <MaterialIcons 
-          name='add' 
-          size={24} 
-          style={styles.modalToggle}
-          onPress={() => setModal(true)} 
+  const [modal, setModal] = useState(false);
+  return (
+    <View style={{ flex: 1 }}>
+      <Modal visible={modal} animationType="slide">
+        <View style={styles.modalContent}>
+          <MaterialIcons
+            name="close"
+            size={24}
+            style={{ ...styles.modalToggle, ...styles.modalClose }}
+            onPress={() => setModal(false)}
           />
-          <FlatList 
-            data={contacts}
-            keyExtractor = {(item) => {
-            return item.id; }}
-            renderItem={( {item} )=> (
-              <TouchableOpacity onPress={()=> navigation.navigate("Chat", item) } >
-                <View style={styles.row}>
-                  <Image source={{ uri: item.image }} style={styles.pic} />
-                  <View>
-                    <View style={styles.nameContainer}>
-                      <Text style={styles.nameTxt} numberOfLines={1} ellipsizeMode="tail">{item.name}</Text>
-                      <Text style={styles.mblTxt}>Mobile</Text>
-                    </View>
-                    <View style={styles.msgContainer}>
-                      <Text style={styles.msgTxt}>{item.status}</Text>
-                    </View>
-                  </View>
-                </View>
-              </TouchableOpacity>
-            )}
-          />
-
+          <AddContact addcontact={addcontact} navigation={navigation} />
         </View>
-    )
+      </Modal>
 
-
-    }
+      <MaterialIcons
+        name="add"
+        size={24}
+        style={styles.modalToggle}
+        onPress={() => setModal(true)}
+      />
+      <FlatList
+        data={contacts}
+        keyExtractor={(item) => {
+          return item.id ? item.id.toString() : "";
+        }}
+        renderItem={({ item }) => (
+          <TouchableOpacity onPress={() => navigation.navigate("Chat", item)}>
+            <View style={styles.row}>
+              <Image source={{ uri: item.image }} style={styles.pic} />
+              <View>
+                <View style={styles.nameContainer}>
+                  <Text
+                    style={styles.nameTxt}
+                    numberOfLines={1}
+                    ellipsizeMode="tail"
+                  >
+                    {item.name}
+                  </Text>
+                  <Text style={styles.mblTxt}>Mobile</Text>
+                </View>
+                <View style={styles.msgContainer}>
+                  <Text style={styles.msgTxt}>{item.status}</Text>
+                </View>
+              </View>
+            </View>
+          </TouchableOpacity>
+        )}
+      />
+    </View>
+  );
+}
 
 const styles = StyleSheet.create({
   row: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    borderColor: '#DCDCDC',
-    backgroundColor: '#fff',
+    flexDirection: "row",
+    alignItems: "center",
+    borderColor: "#DCDCDC",
+    backgroundColor: "#fff",
     borderBottomWidth: 1,
     padding: 10,
   },
@@ -103,41 +167,41 @@ const styles = StyleSheet.create({
     height: 60,
   },
   nameContainer: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
+    flexDirection: "row",
+    justifyContent: "space-between",
     width: 280,
   },
   nameTxt: {
     marginLeft: 15,
-    fontWeight: '800',
-    color: '#222',
+    fontWeight: "800",
+    color: "#222",
     fontSize: 16,
-    width:170,
+    width: 170,
   },
   mblTxt: {
-    fontWeight: '200',
-    color: '#777',
+    fontWeight: "200",
+    color: "#777",
     fontSize: 13,
   },
   msgContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
+    flexDirection: "row",
+    alignItems: "center",
   },
   msgTxt: {
-    fontWeight: '400',
-    color: '#008B8B',
+    fontWeight: "400",
+    color: "#008B8B",
     fontSize: 12,
     marginLeft: 15,
   },
   modalToggle: {
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: "center",
+    alignItems: "center",
     marginBottom: 10,
     borderWidth: 1,
-    borderColor: '#f2f2f2',
+    borderColor: "#f2f2f2",
     padding: 10,
     borderRadius: 10,
-    alignSelf: 'center',
+    alignSelf: "center",
   },
   modalClose: {
     marginTop: 20,
@@ -145,5 +209,5 @@ const styles = StyleSheet.create({
   },
   modalContent: {
     flex: 1,
-  }
-}); 
+  },
+});
