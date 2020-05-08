def76bf42043caa87621421ae449f02ffb3181f0
Shaarif <shaarifkhan999@gmail.com>
2020-05-09 02:06:59 +0500

    add firebase logout

--- frontend/screens/Signout.js
+++ frontend/screens/Signout.js
@@ -5,54 +5,76 @@ import {
   Text,
   TouchableWithoutFeedback,
   Keyboard,
-  Modal
+  Modal,
+  Alert,
 } from "react-native";
 import { globalStyles } from "../styles/global.js";
 import { Formik } from "formik";
 import * as yup from "yup";
-import Icon from 'react-native-vector-icons/FontAwesome';
-import { ActivityIndicator } from 'react-native';
-import Header from '../shared/header'
-import { Input,Button } from 'react-native-elements';
+import Icon from "react-native-vector-icons/FontAwesome";
+import { ActivityIndicator } from "react-native";
+import Header from "../shared/header";
+import { Input, Button } from "react-native-elements";
+import firebase from "../config/firebase";
 
-export default function Chat ({navigation}) {
-    <Header title= {navigation.getParam('name')} />
-    const handlepress=()=>{
-        navigation.navigate("Login")
-        console.log("Logged out")
-    }
+export default function Chat({ navigation }) {
+  <Header title={navigation.getParam("name")} />;
+  const handlepress = () => {
+    firebase
+      .auth()
+      .signOut()
+      .then(function () {
+        navigation.navigate("Login");
+        console.log("Logged out");
+      })
+      .catch(function (err) {
+        Alert.alert(
+          "Error",
+          "Something Went Wrong",
+          [
+            {
+              text: "Retry",
+              onPress: () => {
+                handlepress();
+              },
+            },
+          ],
+          { cancelable: false }
+        );
+      });
+  };
 
-    return(
-        <View style= {styles.container}>
-                <View style={styles.innercontainer}>
-                    <Text style= {styles.titleText}>Are you sure you want to logout?</Text>
-                <Button
-                  buttonStyle={{
-                    marginTop:30,
-                    backgroundColor: "coral",
-                  }}
-                  title="Log Out"
-                  onPress={handlepress}
-                />
-                </View>
-        </View>
-  )
+  return (
+    <View style={styles.container}>
+      <View style={styles.innercontainer}>
+        <Text style={styles.titleText}>Are you sure you want to logout?</Text>
+        <Button
+          buttonStyle={{
+            marginTop: 30,
+            backgroundColor: "coral",
+          }}
+          title="Log Out"
+          onPress={handlepress}
+        />
+      </View>
+    </View>
+  );
 }
 const styles = StyleSheet.create({
-    container: {
-      marginTop: 100,
-      padding: 10,
-      justifyContent: "center",
-    },
-    titleText: {
-        marginTop:100,
-        fontSize: 20,
-    },
-    innercontainer: {
-        marginTop:70,
-        justifyContent:"center",
-        alignContent:"center",
-        marginLeft:50,
-        width: 250
-    }
-  });
+  container: {
+    marginTop: 100,
+    padding: 10,
+    justifyContent: "center",
+  },
+  titleText: {
+    marginTop: 100,
+    fontSize: 20,
+  },
+  innercontainer: {
+    marginTop: 70,
+    justifyContent: "center",
+    alignContent: "center",
+    marginLeft: 50,
+    width: 250,
+  },
+});
