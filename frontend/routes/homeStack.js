f3bcbe3b758d40fc8220fc440e2fd1f508d4e615
fawaz-ansari123 <58271286+fawaz-ansari123@users.noreply.github.com>
2020-05-10 09:55:00 +0500

    Changes by Ansari

--- frontend/routes/homeStack.js
+++ frontend/routes/homeStack.js
@@ -7,11 +7,12 @@ import Signup from "../screens/signup";
 import Chat from "../screens/Chat";
 import Room from "../screens/Room";
 import Addcontact from "../screens/Addcontact";
-import LoginScreen3 from "../screens/login2";
+import Login2 from "../screens/login2";
 import Conversation from "../screens/conversation";
 const screens = {
   Login: {
     screen: Login,
+  
   },
 
   Signup: {
